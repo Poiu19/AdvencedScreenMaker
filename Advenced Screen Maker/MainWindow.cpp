17bd09669cad86eff5b67b459adf8dac6baf03a7
Patryk Merchelski <poiu9119@gmail.com>
2019-02-08 20:39:24 +0100

    Initial

---
+++ Advenced Screen Maker/MainWindow.cpp
@@ -0,0 +1,15 @@
+#include "MainWindow.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Advenced_Screen_Maker::MainWindow mainWindow;
+	Application::Run(%mainWindow);
+}
---
+++ Advenced Screen Maker/MainWindow.h
@@ -0,0 +1,79 @@
+#pragma once
+
+namespace Advenced_Screen_Maker {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MainWindow : public System::Windows::Forms::Form
+	{
+	public:
+		MainWindow(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MainWindow()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  exitButton;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->exitButton = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// exitButton
+			// 
+			this->exitButton->Location = System::Drawing::Point(397, 427);
+			this->exitButton->Name = L"exitButton";
+			this->exitButton->Size = System::Drawing::Size(75, 23);
+			this->exitButton->TabIndex = 0;
+			this->exitButton->Text = L"Exit";
+			this->exitButton->UseVisualStyleBackColor = true;
+			// 
+			// MainWindow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(484, 462);
+			this->Controls->Add(this->exitButton);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::Fixed3D;
+			this->Name = L"MainWindow";
+			this->Text = L"MainWindow";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
